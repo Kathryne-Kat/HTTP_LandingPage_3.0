e94972976cfe770685f39ccfc226e5209688c177
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2023-07-05 15:07:29 +0300

    add fix

--- js/script.js
+++ js/script.js
@@ -1,16 +1,80 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
+// const swiper = new Swiper(".swiper", {
+//   // Optional parameters
+//   direction: "horizontal",
+//   loop: true,
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+// });
+
+/* <div class="swiper-slide" data-swiper-autoplay="2000"></div>; */
+
+var swiperReviews = new Swiper(".swiper-reviews", {
   loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 80,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  },
 
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
-  // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
   },
 });
+
+// var swiperClients = new Swiper(".swiper-clients", {
+//   speed: 400,
+//   spaceBetween: 100,
+//   loop: true,
+//   speed: 1000,
+//   centerInsufficientSlides: true,
+//   autoplay: {
+//     delay: 3000,
+//     disableOnInteraction: false,
+//     stopOnLastSlide: true,
+//   },
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   coverflowEffect: {
+//     rotate: 0,
+//     stretch: 80,
+//     depth: 200,
+//     modifier: 1,
+//     slideShadows: false,
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+// });
